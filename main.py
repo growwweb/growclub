739c0a4cd1af511e58132d795cfdf34b9acb60a8
growwweb <growwweb@gmail.com>
2025-07-29 22:07:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -78,3 +78,15 @@ async def process_contact(message: types.Message, state: FSMContext):
 if __name__ == "__main__":
     from aiogram import executor
     executor.start_polling(dp, skip_updates=True)
+
+
+    app = web.Application()
+    executor.start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host='0.0.0.0',
+        port=int(os.environ.get('PORT', 5000)),
+    )
